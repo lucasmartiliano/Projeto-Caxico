7f049f827f83fad00897bddc5eb9055752d45958
João Lucas Martiliano da Silva <lucasmartiliano16@gmail.com>
2024-11-21 22:57:48 -0300

    Ajuste final

--- script.js
+++ script.js
@@ -14,7 +14,7 @@
                 return;
             }
 
-            const tamanho = `Tamanho escolhido: ${selectedSize.value}`;
+            const tamanho = `\nTamanho escolhido: ${selectedSize.value}`;
 
             // Coleta todas as coberturas marcadas
             const selectedCoberturas = [];
@@ -30,17 +30,83 @@
             }
 
             // Cria a mensagem com as coberturas selecionadas
-            const cobertura = `Coberturas escolhidas: ${selectedCoberturas.join(', ')}`;           
+            const cobertura = `\nCoberturas escolhidas: ${selectedCoberturas.join(', ')}`;           
+            
+            // Coleta todas as frutas marcadas
+            const selectedFrutas = [];
+            const checkboxes1 = document.querySelectorAll('input[name="frutas"]:checked');
+            checkboxes1.forEach(checkbox => {
+                selectedFrutas.push(checkbox.value);
+            });
+
+            // Validação para garantir que pelo menos uma cobertura foi marcada
+            if (selectedFrutas.length === 0) {
+                alert('Por favor, selecione pelo menos uma fruta.');
+                return;
+            }
+
+            // Cria a mensagem com as frutas selecionadas
+            const frutas = `\nFrutas escolhidas: ${selectedFrutas.join(', ')}`;
+
+             // Coleta todas as adicionais marcadas
+
+            const selectedAdicionais = [];
+            const checkboxes2 = document.querySelectorAll('input[name="adicionais"]:checked');
+            checkboxes2.forEach(checkbox => {
+                selectedAdicionais.push(checkbox.value);
+            });
+
+            // Validação para garantir que pelo menos uma cobertura foi marcada
+            if (selectedAdicionais.length === 0) {
+                alert('Por favor, selecione pelo menos uma fruta.');
+                return;
+            }
+
+            // Cria a mensagem com as adicionais selecionadas
+            const adicionais = `\nAdicionais escolhidos: ${selectedAdicionais.join(', ')}`;
+            
+           // Coleta todos os cremes marca
+           const selectedCremes = [];
+           const checkboxes3 = document.querySelectorAll('input[name="cremes"]:checked');
+           checkboxes3.forEach(checkbox => {
+               selectedCremes.push(checkbox.value);
+           });
+       
+           // Validação para garantir que pelo menos uma cobertura foi marcada
+           if (selectedCremes.length === 0) {
+               alert('Por favor, selecione pelo menos um creme.');
+               return;
+           }
+       
+            // Cria a mensagem com as adicionais selecionadas
+            const cremes = `\nCremes escolhidos: ${selectedCremes.join(', ')}`;
+
+           // Coleta todos os extras marca
+           const selectedExtras = [];
+           const checkboxes4 = document.querySelectorAll('input[name="extras"]:checked');
+           checkboxes4.forEach(checkbox => {
+               selectedExtras.push(checkbox.value);
+           });
+       
+           // Validação para garantir que pelo menos uma cobertura foi marcada
+           if (selectedExtras.length === 0) {
+               alert('Por favor, selecione pelo menos um creme.');
+               return;
+           }
+       
+            // Cria a mensagem com as adicionais selecionadas
+            const extras = `\nExtras escolhidos: ${selectedExtras.join(', ')}`;
 
             // Substitua pelo número do WhatsApp desejado
             const phoneNumber = '5582981377291'; 
 
             // Crie o texto da mensagem
-            const text = `Olá, meu nome é ${encodeURIComponent(name)}. ${encodeURIComponent(tamanho)}. ${encodeURIComponent(cobertura)}`;
+            const text = `Olá, meu nome é ${encodeURIComponent(name)}. ${encodeURIComponent(tamanho)}. \n${encodeURIComponent(cobertura)}. ${encodeURIComponent(frutas)}. ${encodeURIComponent(adicionais)}. ${encodeURIComponent(cremes)}. ${encodeURIComponent(extras)}`;
 
             // Construa o link do WhatsApp
             const whatsappURL = `https://wa.me/${phoneNumber}?text=${text}`;
 
             // Redirecione para o WhatsApp
             window.open(whatsappURL, '_blank');
-        });
+        });    
+
